bef4f0b4ca5513db96f4a5db1b8bc550153390ec
Dair Aidarkhanov <36963426+aidarkhanov@users.noreply.github.com>
2018-08-30 17:35:30 +0600

    Moved to ES6 formatting

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var async = require('async');
+const router = require('express').Router()
 
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', (req, res, next) => res.render('index'))
+router.get('/:page', (req, res, next) => res.render(req.params.page, { page: req.params.page }))
 
-router.get('/:page', function(req, res, next) {
-    res.render(req.params.page, {page: req.params.page});
-});
-
-module.exports = router;
+module.exports = router
